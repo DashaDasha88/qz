c892b9a7ba014c07b26719609ddafac06e0c6662
Dasha <dasha.kotova.mail@gmail.com>
2022-01-24 22:42:05 -0500

    add timer functionality

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 import { QuestionService } from '../service/question.service';
 
 @Component({
@@ -15,11 +16,13 @@ export class QuestionComponent implements OnInit {
   counter = 60;
   correctAnswer: number = 0;
   incorrectAnswer: number = 0;
+  interval$ : any;
   constructor(private questionService : QuestionService) { }
 
   ngOnInit(): void {
     this.name = localStorage.getItem("name")!;
     this.getAllQuestions();
+    this.startCounter();
   }
 
   getAllQuestions(){
@@ -47,6 +50,40 @@ export class QuestionComponent implements OnInit {
       this.currentQuestion++;
       this.incorrectAnswer++;
     }
+  }
+
+  startCounter(){
+    this.interval$ = interval(1000) //1000 milliseconds, which is one second
+    .subscribe(val => {
+      this.counter--;
+      if(this.counter === 0){
+        this.currentQuestion++;
+        this.counter = 60;
+        this.points -= 10;
+      }
+    });
+    setTimeout(() => {
+      this.interval$.unsubscribe()
+    }, 600000)
+  }
+
+  stopCounter(){
+    this.interval$.unsubscribe();
+    this.counter = 0;
+  }
+
+  resetCounter(){
+    this.stopCounter();
+    this.counter = 60;
+    this.startCounter();
+  }
+
+  resetQuiz(){
+    this.resetCounter();
+    this.getAllQuestions();
+    this.points = 0;
+    this.counter = 60;
+    this.currentQuestion = 0;
   }
 
 }
